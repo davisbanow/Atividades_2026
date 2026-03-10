dafa9f1e4c700d23add21941c09179c1499f5435
davii <witkowski.davi@escola.pr.gov.br>
2026-03-10 08:23:58 -0300

    Add files via upload

---
+++ ativ02.py
@@ -0,0 +1,12 @@
+idade=input("digite sua idade:")
+idade=int(idade)
+maioridade=idade>=18
+menordeidade=idade<18
+
+if maioridade:
+    print("o usuário é maior de idade")
+
+else:
+    print("o usuário é menor de idade")
+
+
